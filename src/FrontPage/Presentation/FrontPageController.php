b40cd9be01b0a0e4ef683b33ee3b8e52d1f275aa
boonhong <ganboonhong@gmail.com>
2019-10-26 22:01:47 +0800

    2. Bootstrapping

---
+++ src/FrontPage/Presentation/FrontPageController.php
@@ -0,0 +1,19 @@
+<?php declare (strict_types = 1);
+
+namespace SocialNews\FrontPage\Presentation;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * class FrontPageController
+ */
+final class FrontPageController
+{
+    public function show(Request $request): Response
+    {
+        $content = 'Hello ' . $request->get('name', 'visitor');
+
+        return new Response($content);
+    }
+}
